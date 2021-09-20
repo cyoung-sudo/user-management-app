65ec7391d881d99ec6751b2f9a0337961aec663b
Christopher Young <christopheryoung@MacBook123.local>
2021-09-20 14:37:53 -0700

    fixed baseurl for production

--- server/services/render.js
+++ server/services/render.js
@@ -1,9 +1,12 @@
 const axios = require("axios") // Allows network requests
 
+// Dynamic base-url for production
+const url = (process.env.NODE_ENV ? process.env.PRODUCTION_BASEURL : process.env.DEVELOPMENT_BASEURL);
+
 // Functions for router.js
 exports.homeRoute = (req,res) => {
   // Make GET request to /api/users
-  axios.get("http://localhost:3000/api/users")
+  axios.get(`${url}api/users`)
     .then(function(response) {
       res.render("index", {users: response.data})
     })
@@ -18,7 +21,7 @@ exports.add_user = (req,res) => {
 
 exports.update_user = (req,res) => {
   // Make GET request to /api/users
-  axios.get("http://localhost:3000/api/users", {params: {id: req.query.id}})
+  axios.get(`${url}api/users`, {params: {id: req.query.id}})
     .then(function(userdata) {
       res.render("update_user", {user: userdata.data})
     })
